ade1eb4205509ea8e688984844d7e9816f86312f
csamatov96 <csamatov96@gmail.com>
2020-02-16 20:36:20 -0600

    added

--- module/jenkins.tf
+++ module/jenkins.tf
@@ -52,7 +52,7 @@ resource "aws_instance" "jenkins_master" {
         "curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo",
         "sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key",
         "sudo yum install jenkins -y",
-        "sudo service jenkins start",
+        "sudo systemctl jenkins start",
 
 
 	"# These commands below installs docker and configure",
